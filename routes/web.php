31e309e81e642e445becb2013e2d00ae845c76e7
Mohammadsiam <80406919+mohammadsiam98@users.noreply.github.com>
2022-02-20 03:33:26 +0600

    Laravel blade template implemented and popup show

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+//Homepage Route
+Route::get('/', 'App\Http\Controllers\PagesController@homepage')->name('homepage');
+//Homepage Route
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//About Us Route
+Route::get('/about-us', 'App\Http\Controllers\PagesController@about')->name('about');
+//About Us Route
+
+
+//Contact Route
+Route::get('/contact-us', 'App\Http\Controllers\PagesController@contact')->name('contact');
+//Contact Route
